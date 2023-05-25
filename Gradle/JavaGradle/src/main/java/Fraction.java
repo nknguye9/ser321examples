f0d366649d47532b053219e18aaa53a1dc804224
Nguyen <nknguye9@asu.edu>
2023-05-24 21:53:06 -0700

    Added multiply to root.html

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Scanner;
+
 /**
  * Purpose: demonstrate simple Java Fraction class with command line,
  * jdb debugging, and Ant build file.
@@ -43,14 +45,30 @@ public class Fraction {
          // Fraction *frac = [[Fraction alloc] init];
          Fraction frac = new Fraction();
 
-         // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+         // add 2 arguments
+         if (args.length == 2) {
+            int num = 0;
+            int denom = 0;
+            try {
+               num = Integer.parseInt(args[0]);
+               denom = Integer.parseInt(args[1]);
+            } catch (Exception e) {
+               System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
+               System.exit(1);
+            }
+            // set the values
+            frac.setNumerator(num);
+            frac.setDenominator(denom);
 
-         // print it
-         System.out.print("The fraction is: ");
-         frac.print();
-         System.out.println("");
+            //print it
+            System.out.println("The fraction is: ");
+            frac.print();
+            System.out.println("");
+         } else {
+            System.out.println("Exactly 2 arguments should be provided.\n gradle run --args='1 2'");
+         }
+
+
 
       }catch(Exception e) {
          e.printStackTrace();
